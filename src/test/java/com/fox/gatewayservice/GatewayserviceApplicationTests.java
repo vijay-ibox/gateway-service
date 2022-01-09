e820c9f1f8e3e0fee0a209b57fab03d5259e2113
vijay <vijay.ibox@gmail.com>
2022-01-09 19:25:20 +0530

    first commit

---
+++ src/test/java/com/fox/gatewayservice/GatewayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fox.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
